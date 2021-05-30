2c5ab1a14be30ef160dc5d0783cb60c7fc27e4ad
KHJcode <kimstudswcode4485@gmail.com>
2021-05-30 17:46:27 +0900

    'hotfix

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,6 @@ async function bootstrap() {
   );
   app.use(helmet());
   app.use(cookieParser());
-  await app.listen(process.env.PORT || 5000);
+  await app.listen(process.env.$PORT || 5000);
 }
 bootstrap();
